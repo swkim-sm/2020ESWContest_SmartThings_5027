596478a59145eb51bf60386a6d3342acfda6c7f5
yeon0729 <37365249+yeon0729@users.noreply.github.com>
2020-07-27 19:09:47 +0900

    Update arduino_tripleservo.ino

--- arduino/arduino_tripleservo.ino
+++ arduino/arduino_tripleservo.ino
@@ -1,12 +1,15 @@
 #include <SoftwareSerial.h>
 #include <Servo.h> 
+#include "U8glib.h" //OLED 사용을 위한 라이브러리
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C
 
 int servoPin_1 = 8; //서보모터 3번 핀 지정
 int servoPin_2 = 9; //서보모터 3번 핀 지정
 int servoPin_3 = 10; //서보모터 3번 핀 지정
 
 int angle = 90; //서보모터가 움직일 각도 변수
-int signalNum = -1;
+int signalNum = 0;
 
 Servo servo;
 
@@ -16,10 +19,22 @@ void setup() {
 
 void loop() { 
 
+  /*
+  u8g.firstPage();
+  do{
+    showBasicOLED();
+  } while( u8g.nextPage() );*/
+
   if (Serial.available() > 0){
     
     signalNum = Serial.parseInt();
     Serial.println(signalNum);
+
+     u8g.firstPage();
+     do{
+      showBasicOLED();
+      } while( u8g.nextPage() );
+
 
     if (signalNum == 1){
       changeAngle(90, 40, signalNum);
@@ -38,8 +53,6 @@ void loop() {
     }
  
   }
-
-  signalNum = 0;
   
 }
 
@@ -69,6 +82,53 @@ void changeAngle(int startAngle, int endAngle, int motorNum){
       servo.write(angle); 
       delay(15); 
     }
+  }
+  
+}
+
+//OLED 디스플레이 출력
+void showBasicOLED(){
+
+  if (signalNum == -1){
+
+    u8g.setFont(u8g_font_8x13r);
+
+    u8g.setPrintPos(25, 10);
+    u8g.print("Please wear");
+
+    u8g.setScale2x2();
+    u8g.setPrintPos(0, 18);
+    u8g.print("THE MASK");
+    u8g.undoScale();
+
+    u8g.setPrintPos(35, 55);
+    u8g.print("PROPERLY");
+    
+  } else {
+    
+    u8g.setFont(u8g_font_unifont); //폰트 설정
+    u8g.setPrintPos(0, 15); //좌표 이동
+    u8g.print("Input Number: ");
+    u8g.print(signalNum); //문자 외에 변수도 삽입 가능함
+
+    u8g.setFont(u8g_font_8x13r);
+    u8g.setPrintPos(0, 22);
+    
+    u8g.setScale2x2();
+    u8g.print("MOTOR");
+    
+    u8g.setPrintPos(45, 22);
+    u8g.print(signalNum);
+
+    u8g.setPrintPos(55, 22);
+    u8g.print("R");
+    
+    u8g.undoScale();
+
+    u8g.setFont(u8g_font_unifont);
+    u8g.setPrintPos(0, 62);
+    u8g.print("Safe Pass System");
+  
   }
   
 }
