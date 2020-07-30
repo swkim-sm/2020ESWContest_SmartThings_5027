3e9cea10dc9d64b9cb5a1bbe6b07032c99cb1eb1
hrleelab <twinha0204@gmail.com>
2020-07-30 15:28:03 +0900

    complete camera algorithm

--- face-mask-detector/camera.py
+++ face-mask-detector/camera.py
@@ -84,7 +84,7 @@ def getHandGesture(points):
         result = "Four"
     elif fingerCount == 5:
         result = "Five"
-    return result
+    return result, fingerCount
 
 
 # Find out and draw Hand Gesture
@@ -95,6 +95,8 @@ def drawHandGesture(frame, mask):
     thickness = 1
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    max_radius = 0
+    motion_num = -1
     for cnt in contours:
         # 객체 외곽선 그리기
         cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # blue
@@ -104,8 +106,10 @@ def drawHandGesture(frame, mask):
         scale = 0.7
         center = (int(x), int(y))
         radius = int(radius)
-        cv2.circle(frame, center, 2, (0, 255, 0), -1)
-        cv2.circle(frame, center, int(radius*scale), (0, 255, 0), 2)
+        if max_radius < radius:
+            max_radius = radius
+        # cv2.circle(frame, center, 2, (0, 255, 0), -1)
+        # cv2.circle(frame, center, int(radius*scale), (0, 255, 0), 2)
 
         # 손가락 개수를 세기 위한 원 그리기
         cImg = np.zeros(mask.shape, np.uint8)
@@ -123,14 +127,14 @@ def drawHandGesture(frame, mask):
                 points.append(p1)
 
         # 손가락 마디(선) 그리기
-        for i in range(len(points)):
-            cv2.line(frame, center, points[i], (0, 255, 0), 3)
+        # for i in range(len(points)):
+            # cv2.line(frame, center, points[i], (0, 255, 0), 3)
 
         # 제스처 출력
-        text = getHandGesture(points)
+        text, motion_num = getHandGesture(points)
         cv2.putText(frame, text, center, font, fontScale, color, thickness)
 
-    return frame
+    return frame, motion_num, max_radius
 
 
 def detect_and_predict_mask(frame, faceNet, maskNet):
@@ -210,8 +214,7 @@ args = vars(ap.parse_args())
 # load our serialized face detector model from disk
 print("[INFO] loading face detector model...")
 prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
-weightsPath = os.path.sep.join([args["face"],
-    "res10_300x300_ssd_iter_140000.caffemodel"])
+weightsPath = os.path.sep.join([args["face"], "res10_300x300_ssd_iter_140000.caffemodel"])
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 # load the face mask detector model from disk
@@ -225,7 +228,9 @@ time.sleep(2.0)
 
 # loop over the frames from the video stream
 mask_loop = False
-hand_loop = False
+mask_loop_end = False
+motion_loop = False
+motion_loop_end = False
 while True:
     # -------- | 영상 입력 받기 | ------------------------------------------------------
     # grab the frame from the threaded video stream and resize it
@@ -235,69 +240,99 @@ while True:
     frame = cv2.flip(frame, 1)
 
     # -------- | 얼굴-마스크 인식 | ----------------------------------------------------
-    # 얼굴 ROI
-    face_start_x, face_end_x = 300, 700
-    face_start_y, face_end_y = 100, 600
-    vertices = [[face_start_x, face_start_y], [face_end_x, face_start_y], [face_end_x, face_end_y], [face_start_x, face_end_y]]
-    frame_face = region_of_interest(frame, vertices, color3=(255, 255, 255), color1=255)
+    if not mask_loop_end:
+        # 얼굴 ROI
+        face_start_x, face_end_x = 300, 700
+        face_start_y, face_end_y = 100, 600
+        vertices = [[face_start_x, face_start_y], [face_end_x, face_start_y], [face_end_x, face_end_y], [face_start_x, face_end_y]]
+        frame_face = region_of_interest(frame, vertices, color3=(255, 255, 255), color1=255)
 
-    # detect faces in the frame and determine if they are wearing a
-    # face mask or not
-    (locs, preds) = detect_and_predict_mask(frame_face, faceNet, maskNet)
+        # detect faces in the frame and determine if they are wearing a
+        # face mask or not
+        (locs, preds) = detect_and_predict_mask(frame_face, faceNet, maskNet)
 
-    # loop over the detected face locations and their corresponding
-    # locations
-    mask_flag = False
-    for (box, pred) in zip(locs, preds):
-        # unpack the bounding box and predictions
-        (startX, startY, endX, endY) = box
-        (mask, withoutMask) = pred
+        # loop over the detected face locations and their corresponding
+        # locations
+        mask_flag = False
+        for (box, pred) in zip(locs, preds):
+            # unpack the bounding box and predictions
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
 
-        # determine the class label and color we'll use to draw
-        # the bounding box and text
-        label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-        if mask > withoutMask:
-            mask_flag = True
-        # include the probability in the label
-        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+            # determine the class label and color we'll use to draw
+            # the bounding box and text
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+            if mask > withoutMask:
+                mask_flag = True
+            # include the probability in the label
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-        # display the label and bounding box rectangle on the output
-        # frame
-        cv2.putText(frame, label, (startX, startY - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-    # 시간 구하기
-    if mask_flag:
-        if not mask_loop:
-            mask_loop = True
-            start = time.time()
+            # display the label and bounding box rectangle on the output
+            # frame
+            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        # 시간 구하기
+        if mask_flag:
+            if not mask_loop:
+                mask_loop = True
+                start = time.time()
+            else:
+                during = time.time() - start
+                cv2.putText(frame, str(during), (startX, startY + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                if during > 3:
+                    mask_loop = False
+                    mask_loop_end = True
+                    print("얼굴 마스크 인식 완료")
         else:
-            during = time.time() - start
-            cv2.putText(frame, str(during), (startX, startY + 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            if during > 3:
-                mask_loop = False
-                hand_loop = True
-    else:
-        mask_loop = False
+            mask_loop = False
 
     # -------- | 모션 인식 - 왼손 & 오른손 | ------------------------------------------------
-    # 손 ROI
-    left_start_x, left_end_x = 10, 330
-    left_start_y, left_end_y = 300, 700
-    right_start_x, right_end_x = 670, 990
-    right_start_y, right_end_y = 300, 700
-    vertices_left = [[left_start_x, left_start_y], [left_end_x, left_start_y], [left_end_x, left_end_y], [left_start_x, left_end_y]]
-    vertices_right = [[right_start_x, right_start_y], [right_end_x, right_start_y], [right_end_x, right_end_y], [right_start_x, right_end_y]]
-    frame_hand_left = region_of_interest(frame, vertices_left, color3=(255, 255, 255), color1=255)
-    frame_hand_right = region_of_interest(frame, vertices_right, color3=(255, 255, 255), color1=255)
-    # Skin detect and Noise reduction
-    mask_left = detectSkin(frame_hand_left)
-    mask_right = detectSkin(frame_hand_right)
-    # Find out and Draw Hand Gesture
-    frame = drawHandGesture(frame, mask_left)
-    frame = drawHandGesture(frame, mask_right)
+    if mask_loop_end and not motion_loop_end:
+        # 손 ROI
+        left_start_x, left_end_x = 10, 330
+        left_start_y, left_end_y = 300, 700
+        right_start_x, right_end_x = 670, 990
+        right_start_y, right_end_y = 300, 700
+        vertices_left = [[left_start_x, left_start_y], [left_end_x, left_start_y], [left_end_x, left_end_y], [left_start_x, left_end_y]]
+        vertices_right = [[right_start_x, right_start_y], [right_end_x, right_start_y], [right_end_x, right_end_y], [right_start_x, right_end_y]]
+        frame_hand_left = region_of_interest(frame, vertices_left, color3=(255, 255, 255), color1=255)
+        frame_hand_right = region_of_interest(frame, vertices_right, color3=(255, 255, 255), color1=255)
+        # Skin detect and Noise reduction
+        mask_left = detectSkin(frame_hand_left)
+        mask_right = detectSkin(frame_hand_right)
+        # Find out and Draw Hand Gesture
+        frame, left_result, left_radius = drawHandGesture(frame, mask_left)
+        frame, right_result, right_radius = drawHandGesture(frame, mask_right)
+        # motion time
+        motion_flag = False
+        result = -1
+        if left_radius > right_radius:
+            if left_result in (1, 2, 3):
+                result = left_result
+                motion_flag = True
+        else:
+            if right_result in (1, 2, 3):
+                result = right_result
+                motion_flag = True
+        # 시간 구하기
+        if motion_flag:
+            if not motion_loop:
+                motion_loop = True
+                start = time.time()
+                motion_num = result
+            elif motion_num == result:
+                during = time.time() - start
+                cv2.putText(frame, str(during), (500, 500), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                if during > 3:
+                    motion_loop = False
+                    motion_loop_end = True
+                    print("모션인식 완료")
+                    print("모션 : {motion}".format(motion=motion_num))
+            else:
+                motion_loop = False
+        else:
+            motion_loop = False
 
     # -------- | ROI 영역 그리기 | ----------------------------------------------------------
     frame = cv2.rectangle(frame, (300, 100), (700, 600), (255, 255, 0), 3)
@@ -312,6 +347,10 @@ while True:
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
         break
+    elif key == ord("r"):
+        mask_loop_end = False
+        motion_loop_end = False
+
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
