802bb55147cd3b5d2196185a010f6b237ae4212d
swkim-sm <58676931+swkim-sm@users.noreply.github.com>
2020-09-14 10:16:44 +0900

    Add files via upload

--- face-mask-detector/keyboard.py
+++ face-mask-detector/keyboard.py
@@ -12,103 +12,31 @@ import imutils
 import time
 import cv2
 import os
+from yolo import YOLO
 
 
-# Skin detect and Noise reduction
-def detectSkin(original):
-    # Color binarization
-    ycbcr = cv2.cvtColor(src=original, code=cv2.COLOR_RGB2YCrCb)
-    lower = np.array([0, 85, 135])
-    upper = np.array([255, 135, 180])
-    mask = cv2.inRange(ycbcr, lower, upper)
+#손의 중심 포인트가 버튼 ROI에 포함되는지 확인하는 함수
+def check_ROI(point, btn_roi):
 
-    # Noise reduction - Median Filter
-    mask = cv2.medianBlur(src=mask, ksize=7)
-    element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(7, 7))
-    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_CLOSE, kernel=element)
-    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_OPEN, kernel=element)
-    return mask
+    if point[0]+500 >= btn_roi[0] and point[0]+500 <= btn_roi[2]: X_CORRECT = True
+    else: X_CORRECT = False
 
+    if point[1] >= btn_roi[1] and point[1] <= btn_roi[3]: Y_CORRECT = True
+    else: Y_CORRECT = False
 
-# 손가락 개수와 손가락 사이 각을 이용한 제스쳐 인식
-def getHandGesture(points):
-    fingerCount = len(points) - 1
-    result = ""
-    if fingerCount == 0:
-        result = "Rock"  # 주먹
-    elif fingerCount == 1:
-        result = "One"  # 손가락 하나
-    elif fingerCount == 2:
-        result = "Two"  # 손가락 둘
-    elif fingerCount == 3:
-        result = "Three"
-    elif fingerCount == 4:
-        result = "Four"
-    elif fingerCount == 5:
-        result = "Five"
-    return result, fingerCount
+    if X_CORRECT and Y_CORRECT:
+        return True
+    else:
+        return False
 
+#버튼 rectangle 생성하는 함수 - clicked 여부에 따라 분류
+def make_button(x, y, width, height, text, clicked):
 
-# Find out and draw Hand Gesture
-def drawHandGesture(frame, mask, keyboard_x, keyboard_y):
-    font = cv2.FONT_HERSHEY_COMPLEX
-    fontScale = 1
-    color = (0, 255, 255)
-    thickness = 1
-
-    mask = cv2.medianBlur(src=mask, ksize=7)
-    element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(7, 7))
-    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_CLOSE, kernel=element)
-    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_OPEN, kernel=element)
-
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    max_radius = 0
-    motion_num = -1
-    for cnt in contours:
-        # 객체 외곽선 그리기
-        cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # blue
-
-        # 손바닥 중심과 반지름 찾기
-        (x, y), radius = cv2.minEnclosingCircle(cnt)
-        scale = 0.7
-        center = (int(x), int(y))
-        radius = int(radius)
-        if radius < 100:
-            continue
-        if max_radius < radius:
-            max_radius = radius
-        # cv2.circle(frame, center, 2, (0, 255, 0), -1)
-        # cv2.circle(frame, center, int(radius*scale), (0, 255, 0), 2)
-
-        # 손가락 개수를 세기 위한 원 그리기
-        cImg = np.zeros(mask.shape, np.uint8)
-        cv2.circle(cImg, center, int(radius*scale), 255)
-
-        # 원의 외곽선을 저장할 벡터
-        circleContours, hierarchy = cv2.findContours(cImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # 원의 외곽선을 따라 돌며 mask의 값이 0에서 1로 바뀌는 지점 확인
-        points = []
-        for i in reversed(range(1, len(circleContours[0]))):
-            p1 = (circleContours[0][i][0][0], circleContours[0][i][0][1])
-            p2 = (circleContours[0][i-1][0][0], circleContours[0][i-1][0][1])
-            if mask[p1[1], p1[0]] == 0 and mask[p2[1], p2[0]] > 1:
-                points.append(p1)
-
-        # 손가락 마디(선) 그리기
-        for i in range(len(points)):
-            cv2.line(frame, center, points[i], (0, 255, 0), 3)
-
-        # 제스처 출력
-        text, motion_num = getHandGesture(points)
-        cv2.putText(frame, text, center, font, fontScale, color, thickness)
-
-    return frame, motion_num, max_radius
-
-
-def make_button(x, y, width, height, text):
-    th = 3 # thickness
-    cv2.rectangle(frame, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
+    th = 3  # thickness
+    if clicked:
+        cv2.rectangle(frame, (x + th, y + th), (x + width - th, y + height - th), (0, 255, 255), -1)
+    else:
+        cv2.rectangle(frame, (x + th, y + th), (x + width - th, y + height - th), (255, 0, 0), th)
 
     # Text settings
     font_letter = cv2.FONT_HERSHEY_PLAIN
@@ -120,8 +48,25 @@ def make_button(x, y, width, height, text):
     text_y = int((height + height_text) / 2) + y
     cv2.putText(frame, text, (text_x, text_y), font_letter, font_scale, (255, 0, 0), font_th)
 
-    return frame[x + th : x + width - th, y + th : y + height - th]
+    return x, y, x + width, y + height, text
 
+
+def hand_detection(keyboard_roi, results):
+    center = (0, 0)
+    area = 0
+    for detection in results:
+        id, name, confidence, x, y, w, h = detection
+        area = w*h
+        # draw a bounding box rectangle and label on the image
+        color = (0, 255, 255)
+        cv2.rectangle(keyboard_roi, (x, y), (x + w, y + h), color, 2)
+        center = (int((x + x + w)/2), int((y + y + h)/2)) #Detection 중심 계산
+        cv2.circle(keyboard_roi, center, 3, (0, 0, 255), -1) #Detection 중심 포인트 표시
+        text = "%s (%s)" % (name, round(confidence, 2))
+        cv2.putText(keyboard_roi, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            
+    return center, area
+    
 def detect_and_predict_mask(frame, faceNet, maskNet):
 	# grab the dimensions of the frame and then construct a blob
 	# from it
@@ -185,15 +130,27 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--face", type=str,
-	default="face_detector",
-	help="path to face detector model directory")
-ap.add_argument("-m", "--model", type=str,
-	default="mask_detector.model",
-	help="path to trained face mask detector model")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
+ap.add_argument("-f", "--face", type=str, default="face_detector", help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str, default="mask_detector.model", help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5, help="minimum probability to filter weak detections")
+ap.add_argument('-n', '--network', default="tiny", help='Network Type: normal / tiny / prn')
+ap.add_argument('-d', '--device', default=0, help='Device to use')
+ap.add_argument('-s', '--size', default=416, help='Size for yolo')
+ap.add_argument('-hc', '--handconfidence', default=0.2, help='Confidence for yolo')
 args = vars(ap.parse_args())
+#args = ap.parse_args()
+
+if args["network"] == "normal":
+    print("loading yolo...")
+    yolo = YOLO("models/cross-hands.cfg", "models/cross-hands.weights", ["hand"])
+elif args["network"] == "prn":
+    print("loading yolo-tiny-prn...")
+    yolo = YOLO("models/cross-hands-tiny-prn.cfg", "models/cross-hands-tiny-prn.weights", ["hand"])
+else:
+    print("loading yolo-tiny...")
+    yolo = YOLO("models/cross-hands-tiny.cfg", "models/cross-hands-tiny.weights", ["hand"])
+yolo.size = int(args["size"])
+yolo.confidence = float(args["handconfidence"])
 
 # load our serialized face detector model from disk
 print("[INFO] loading face detector model...")
@@ -216,9 +173,13 @@ wearing_mask = False
 motion_end = False
 mask_flag = False
 
+# flag for clicking button
+before_area = 0
+click_check_ratio = 1.4
+
 while True:
     frame = vs.read()
-    frame = imutils.resize(frame, width=1100)
+    frame = imutils.resize(frame, 1000)
     frame = cv2.flip(frame, 1)
     # mask 미착용 또는 확인 단계 
     if not wearing_mask:
@@ -230,6 +191,7 @@ while True:
 
             label = "Mask" if mask > withoutMask else "No Mask"
             color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
 
             if label == "Mask" and not mask_flag: # 마스크 착용 시간 재기
                 mask_flag = True
@@ -253,26 +215,42 @@ while True:
         # keyboard 생성
         keyboard_x = 600
         keyboard_y = 200
-        keyboard_roi = frame[0:1000, 500:1100]
-        hand_roi = detectSkin(keyboard_roi)
-        frame, hand_result, hand_radius = drawHandGesture(frame, hand_roi, keyboard_x, keyboard_y)
-        display = cv2.rectangle(frame, (keyboard_x+3, keyboard_y+3), (keyboard_x+400-3, keyboard_y-150-3), (255, 0, 0), 3)
-        btn_A = make_button(keyboard_x, keyboard_y, 200, 200, "A")
-        btn_B = make_button(keyboard_x+200, keyboard_y, 200, 200, "B")
-        btn_C = make_button(keyboard_x, keyboard_y+200, 200, 200, "C")
-        btn_D = make_button(keyboard_x+200, keyboard_y+200, 200, 200, "D")
-        btn_end = make_button(keyboard_x, keyboard_y+400, 400, 150, "End")
-        
-        # hand gesture 로 keyboard 클릭
-        #cv2.rectangle(frame, (keyboard_x, keyboard_y), (keyboard_x+400, keyboard_y+600), (255, 255, 0), 2)
-        
-        
+        keyboard_roi = frame[0:1100, 500:1100]
 
+        click_flag = False
+        width, height, inference_time, results = yolo.inference(keyboard_roi)
+        center, area = hand_detection(keyboard_roi, results)
+        btn_list = []
+        btn_all = make_button(keyboard_x, keyboard_y-180, 400, 180, "All", False)
+        btn_A = make_button(keyboard_x, keyboard_y, 200, 180, "A", False)
+        btn_B = make_button(keyboard_x+200, keyboard_y, 200, 180, "B", False)
+        btn_C = make_button(keyboard_x, keyboard_y+180, 200, 180, "C", False)
+        btn_D = make_button(keyboard_x+200, keyboard_y+180, 200, 180, "D", False)
+        btn_left = make_button(keyboard_x, keyboard_y+360, 145, 180, "<", False)
+        btn_stop = make_button(keyboard_x+145, keyboard_y+360, 110, 180, "-", False)
+        btn_right = make_button(keyboard_x+255, keyboard_y+360, 145, 180, ">", False)
 
-## To do
-#### 버튼마다 버튼안에 손바닥 들어가면 색깔 바뀌게
-#### 버튼에서 손가락이 0이면 클릭하기
-        
+        btn_list.append(btn_all)
+        btn_list.append(btn_A)
+        btn_list.append(btn_B)
+        btn_list.append(btn_C)
+        btn_list.append(btn_D)
+        btn_list.append(btn_left)
+        btn_list.append(btn_stop)
+        btn_list.append(btn_right)
+
+        #손 면적 여부를 통한 클릭 여부 확인
+        if (area >= int(before_area * click_check_ratio)): #클릭 후 손 펴는 동작으로 면적이 커졌을 시
+            click_flag = True
+
+        if click_flag: #클릭 발생 시
+            for btn in range(0, len(btn_list)):
+                if check_ROI(center, btn_list[btn]):
+                    make_button(btn_list[btn][0], btn_list[btn][1], btn_list[btn][2]-btn_list[btn][0], btn_list[btn][3]-btn_list[btn][1], btn_list[btn][4], True)
+                    print(btn)
+             
+        before_area = area
+        click_flag = False
 
 
 
@@ -282,64 +260,6 @@ while True:
     if key == ord("q"):
         break
 
-'''
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=1000)
-
-    if not wearing_mask:
-
-        # detect faces in the frame and determine if they are wearing face mask or not
-        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
-        # loop over the detected face locations and their corresponding locations
-        for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
-            (startX, startY, endX, endY) = box
-            (mask, withoutMask) = pred
-
-            # determine the class label and color we'll use to draw the bounding box and text
-            label = "Mask" if mask > withoutMask else "No Mask"
-            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-            # include the probability in the label
-            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-            # display the label and bounding box rectangle on the output frame
-            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-    else:
-        # detect faces in the frame and determine if they are wearing a
-        # face mask or not
-        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
-
-        # loop over the detected face locations and their corresponding locations
-        for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
-            (startX, startY, endX, endY) = box
-            (mask, withoutMask) = pred
-
-            # determine the class label and color we'll use to draw the bounding box and text
-            label = "Mask" if mask > withoutMask else "No Mask"
-            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-
-            # include the probability in the label
-            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-            # display the label and bounding box rectangle on the output frame
-            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-'''
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
